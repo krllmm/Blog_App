b1ffe2f8bedbeda501f9204b559015116a59dd57
spumescent <kirilenkich@gmail.com>
2023-09-11 23:06:34 +0300

    Update blog_dump.sql
    new db dump since the last version got more content.

--- db_dump/blog_dump.sql
+++ db_dump/blog_dump.sql
@@ -29,7 +29,7 @@ CREATE TABLE `article_tag` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `article_tag` (
 
 LOCK TABLES `article_tag` WRITE;
 /*!40000 ALTER TABLE `article_tag` DISABLE KEYS */;
-INSERT INTO `article_tag` VALUES (1,7,1,NULL,NULL),(2,7,2,NULL,NULL),(3,8,1,NULL,NULL),(4,8,2,NULL,NULL),(7,1,1,NULL,NULL);
+INSERT INTO `article_tag` VALUES (1,7,1,NULL,NULL),(2,7,2,NULL,NULL),(3,8,1,NULL,NULL),(4,8,2,NULL,NULL),(7,1,1,NULL,NULL),(8,9,2,NULL,NULL),(9,9,4,NULL,NULL),(13,10,1,NULL,NULL),(14,10,2,NULL,NULL),(15,10,4,NULL,NULL),(16,10,6,NULL,NULL),(19,2,1,NULL,NULL),(20,11,2,NULL,NULL),(21,11,4,NULL,NULL),(22,12,2,NULL,NULL),(23,13,1,NULL,NULL),(24,13,2,NULL,NULL),(25,13,4,NULL,NULL),(26,14,1,NULL,NULL);
 /*!40000 ALTER TABLE `article_tag` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,7 @@ CREATE TABLE `articles` (
   PRIMARY KEY (`id`),
   KEY `articles_category_id_foreign` (`category_id`),
   CONSTRAINT `articles_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `articles` (
 
 LOCK TABLES `articles` WRITE;
 /*!40000 ALTER TABLE `articles` DISABLE KEYS */;
-INSERT INTO `articles` VALUES (1,'title','content','default_article_preview_image.png',3,NULL,'2023-09-03 17:38:44'),(2,'title3','content3','adobe@2x.png',1,'2023-09-02 05:43:57','2023-09-02 05:43:57'),(3,'vbbmbm','vbnmvbnv','ethereum@2x.png',1,'2023-09-02 06:04:59','2023-09-02 06:04:59');
+INSERT INTO `articles` VALUES (2,'title3edit','content3edit','images.jpg',6,'2023-09-02 05:43:57','2023-09-08 04:50:46'),(10,'test ui','test content ui','donut1.png',6,'2023-09-06 05:32:53','2023-09-09 07:25:44'),(11,'Long title for the article that makes sence','Science is an enterprise that should be cherished as an activity of the free human mind. Because it transforms who we are, how we live, and it gives us an understanding of our place in the universe. The universe is large and old, and the ingredients for life as we know it are everywhere, so there\'s no reason to think that Earth would be unique in that regard. Whether of not the life became intelligent is a different question, and we\'ll see if we find that. If you get asteroids about a kilometer in size, those are large enough and carry enough energy into our system to disrupt transportation, communication, the food chains, and that can be a really bad day on Earth. For me, the most fascinating interface is Twitter. \r\nI have odd cosmic thoughts every day and I realized I could hold them to myself or share them with people who might be interested.','blurred_car_on_road.jpg',5,'2023-09-08 04:31:31','2023-09-08 04:46:26'),(12,'Article 4','Long long content for article number 4 so it is cut out for the index page','Pyramid-1.png',3,'2023-09-10 11:45:03','2023-09-10 11:45:03'),(13,'Article 5','Another long content for the article that has a default picture','default_article_preview_image.png',6,'2023-09-10 11:45:50','2023-09-10 11:45:50'),(14,'Article about space cats','New article talks about space cats :)','cat.jpg',3,'2023-09-10 11:46:33','2023-09-10 11:46:33');
 /*!40000 ALTER TABLE `articles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,7 +87,7 @@ CREATE TABLE `categories` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `categories_category_unique` (`category`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +96,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'travel',NULL,NULL),(3,'ideas','2023-09-03 15:56:52','2023-09-03 15:56:52');
+INSERT INTO `categories` VALUES (1,'travel',NULL,NULL),(3,'ideas','2023-09-03 15:56:52','2023-09-03 15:56:52'),(5,'food','2023-09-06 04:35:26','2023-09-06 04:35:26'),(6,'news','2023-09-06 04:39:40','2023-09-06 04:39:40');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -224,7 +224,7 @@ CREATE TABLE `tags` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -233,7 +233,7 @@ CREATE TABLE `tags` (
 
 LOCK TABLES `tags` WRITE;
 /*!40000 ALTER TABLE `tags` DISABLE KEYS */;
-INSERT INTO `tags` VALUES (1,'longread','2023-09-03 12:47:48','2023-09-03 12:47:48'),(2,'new','2023-09-03 12:49:29','2023-09-03 12:49:29'),(4,'food','2023-09-03 15:57:12','2023-09-03 15:57:12');
+INSERT INTO `tags` VALUES (1,'longread','2023-09-03 12:47:48','2023-09-03 12:47:48'),(2,'new','2023-09-03 12:49:29','2023-09-03 12:49:29'),(4,'food','2023-09-03 15:57:12','2023-09-03 15:57:12'),(6,'test','2023-09-06 04:52:28','2023-09-06 04:52:28');
 /*!40000 ALTER TABLE `tags` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -276,4 +276,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-03 23:40:44
+-- Dump completed on 2023-09-11 23:05:17
